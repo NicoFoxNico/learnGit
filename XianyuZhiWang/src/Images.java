6285d7682e49cafee26ab2ffd0e3e7e3ce6434e4
admin <jinhuihui0807@163.com>
2024-01-13 19:31:00 +0800

    还没开始

---
+++ XianyuZhiWang/src/Images.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Images {
+    //背景图
+    public static ImageIcon background;
+    //
+
+    static {
+        background = new ImageIcon("img/background2.jpg");
+    }
+
+
+}
--- XianyuZhiWang/src/XianyuZhiWang.java
+++ XianyuZhiWang/src/XianyuZhiWang.java
@@ -1,10 +1,11 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import java.awt.*;
 
 public class XianyuZhiWang {
 
     //使用常量定义界面尺寸 长641  宽479
-    public static final int WIDTH = 641;
-    public static final int HEIGHT = 479;
+    public static final int WIDTH = 750;
+    public static final int HEIGHT = 1433;
 
     //常量定义游戏状态 开始=0 运行=1 暂停=2 结束=-1
     public static final int START = 0;
@@ -14,9 +15,20 @@ public class XianyuZhiWang {
     protected int state = START;
 
 
+    public void paint(Graphics graphics){
 
+    }
 
     public static void main(String[] args) {
+
         JFrame frame = new JFrame("咸鱼之王");
+        XianyuZhiWang xyzw = new XianyuZhiWang();
+   //     frame.add();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH+6,HEIGHT+80);
+        frame.setResizable(true);
+        frame.setVisible(true);
+
+
     }
 }
